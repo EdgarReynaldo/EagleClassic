01c8afffa57d66dfaab06cd5862be91bb7431a6b
Edgar Reynaldo <edgarreynaldo@members.allegro.cc>
2017-11-12 14:10:33 -0600

    build script for examples

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-export A4INCDIR=$(ALLEGRO_DIR)/include
-g++ -Wall -g -o bin\Eagle4Demo.exe -DALLEGRO_STATICLINK -I include $(A4INCDIR) src\TestPrograms\Eagle4WidgetDemo.cpp -L lib $(A4LNKDIR) -leagle4-debug -lalleg
+if ($(ALLEGRO_DIR) == "")
+   export ALLEGRO_DIR=/usr/local
+export A4INCDIR=-I $(ALLEGRO_DIR)/include
+export A4LNKDIR=-L $(ALLEGRO_DIR)/lib
+
+g++ -Wall -g -o bin\Eagle4Demo -I include $(A4INCDIR) src\TestPrograms\Eagle4WidgetDemo.cpp -L lib $(A4LNKDIR) -leagle4-debug -lalleg
+
+g++ -Wall -g -o bin\Eagle4Animation -I include $(A4INCDIR) src\TestPrograms\Animation.cpp -L lib $(A4LNKDIR) -leagle4-debug -lalleg
